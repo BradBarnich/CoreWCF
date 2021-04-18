e559765accc279a1f331645cf39c33de6c006d4e
Brad Barnich <brad@careevolution.com>
2021-04-18 12:31:03 -0400

    stash

--- src/CoreWCF.Http/src/CoreWCF/BasicHttpBinding.cs
+++ src/CoreWCF.Http/src/CoreWCF/BasicHttpBinding.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using CoreWCF.Channels;
 
 namespace CoreWCF
@@ -19,6 +20,16 @@ namespace CoreWCF
         }
 
         internal WSMessageEncoding MessageEncoding { get; set; } = BasicHttpBindingDefaults.MessageEncoding;
+
+        [Obsolete]
+        public BasicHttpSecurity Security
+        {
+            get { return _basicHttpSecurity; }
+            set
+            {
+                _basicHttpSecurity = value ?? throw DiagnosticUtility.ExceptionUtility.ThrowHelperArgumentNull(nameof(value));
+            }
+        }
 
         internal override BasicHttpSecurity BasicHttpSecurity => _basicHttpSecurity;
 
